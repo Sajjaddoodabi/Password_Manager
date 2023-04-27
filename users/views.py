86bcbf789d2e7370881a94dff2259248698163da
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-04-27 20:39:32 +0430

    add user view

--- users/views.py
+++ users/views.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.response import Response
 
-from users.serializers import UserRegisterSerializer
+from users.serializers import UserRegisterSerializer, UserSerializer
 
 
 class RegisterView(CreateAPIView):
     serializer_class = UserRegisterSerializer
     queryset = User.objects.all()
 
+
+class UserView(RetrieveAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        if self.request.user.is_authenticated:
+            return self.request.user
+        return AuthenticationFailed('Unauthenticated!')
