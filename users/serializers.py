da6b66c1ac2a822723c0cdbc0c39c93191218ede
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-04-27 20:14:06 +0430

    add User and User register serializer

---
+++ users/serializers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_active')
+        read_only_fields = ('id', 'is_active')
+
+
+class UserRegistrationView(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    confirm_password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'confirm_password')
