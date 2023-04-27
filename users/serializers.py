f2b19638169c994a4298ef2b329fe6602c2d77c4
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-04-27 20:16:57 +0430

    add User register view and modified register serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 
@@ -9,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'is_active')
 
 
-class UserRegistrationView(serializers.ModelSerializer):
+class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True,
         required=True,
@@ -25,3 +26,32 @@ class UserRegistrationView(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'password', 'confirm_password')
+
+    def validate(self, attrs):
+        # check if password is strong enough
+        validate_password(attrs.get('password'))
+
+        # checking if password and confirm password are the same
+        if attrs.get('password') != attrs.get('confirm_password'):
+            raise serializers.ValidationError('password and confirm password does NOT match!')
+
+        return attrs
+
+    @staticmethod
+    def clean_validated_data(validated_data):
+        # deleting confirm password because we dont need it anymore
+        validated_data.pop('confirm_password')
+
+        return validated_data
+
+    def create(self, validated_data):
+        # create user with given data
+        user = self.Meta.model.objects.create(**self.clean_validated_data(validated_data))
+
+        # set password for user
+        user.set_password(validated_data.get('password'))
+
+        # save user into database
+        user.save()
+
+        return user
--- users/views.py
+++ users/views.py
@@ -1,2 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+
+from users.serializers import UserRegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+    queryset = User.objects.all()
 
