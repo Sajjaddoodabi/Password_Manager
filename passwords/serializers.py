233f7b180f1b8a1bfd47c61a641c783b8a1cc4ba
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-04-30 22:33:39 +0430

    add password serializer rename password serializer to Full password serializer

--- passwords/serializers.py
+++ passwords/serializers.py
@@ -3,10 +3,24 @@ from rest_framework import serializers
 from passwords.models import Passwords
 
 
-class PasswordSerializer(serializers.ModelSerializer):
+class FullPasswordSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = Passwords
-        fields = ('id', 'user', 'site_name', 'site_url', 'username', 'email', 'password', 'date_created', 'last_date_edited')
+        fields = (
+            'id', 'user', 'site_name', 'site_url', 'username', 'email', 'password', 'date_created', 'last_date_edited')
+        read_only_fields = ('id', 'date_created', 'last_date_edited')
+
+
+class PasswordSerializer(serializers.ModelSerializer):
+    user_password = serializers.CharField(
+        required=True,
+        write_only=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = Passwords
+        fields = ('id', 'site_name', 'site_url', 'username', 'email', 'password', 'user_password')
         read_only_fields = ('id', 'date_created', 'last_date_edited')
