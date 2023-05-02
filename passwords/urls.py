d35914a26a62758d172f0c77674e75e3dc6b0921
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-05-02 16:17:38 +0430

    add delete password url

--- passwords/urls.py
+++ passwords/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AddPasswordView, UpdatePasswordView
+from .views import AddPasswordView, UpdatePasswordView, DeletePasswordView
 
 urlpatterns = [
     path('add/', AddPasswordView.as_view(), name='add_password'),
     path('<int:id>/update/', UpdatePasswordView.as_view(), name='update_password'),
+    path('<int:id>/delete/', DeletePasswordView.as_view(), name='delete_password'),
 ]
