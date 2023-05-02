685b32554d4fb7a9cebe663dd18088b14c65f9ec
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-05-02 16:29:42 +0430

    add password list url

--- passwords/urls.py
+++ passwords/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddPasswordView, UpdatePasswordView, DeletePasswordView
+from .views import AddPasswordView, UpdatePasswordView, DeletePasswordView, PasswordListAPIView
 
 urlpatterns = [
     path('add/', AddPasswordView.as_view(), name='add_password'),
+    path('password_list/', PasswordListAPIView.as_view(), name='password_list'),
     path('<int:id>/update/', UpdatePasswordView.as_view(), name='update_password'),
     path('<int:id>/delete/', DeletePasswordView.as_view(), name='delete_password'),
 ]
