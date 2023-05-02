67427179359827cb51383f9f34dfda833569399c
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-05-02 16:07:55 +0430

    add update password view

--- passwords/views.py
+++ passwords/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -34,7 +34,7 @@ class AddPasswordView(APIView):
                 response = {'detail': 'user password invalid!'}
                 return Response(response)
 
-            # checking if the exact informations are already in the database
+            # checking if the exact information are already in the database
             is_exist = Passwords.objects.filter(
                 user=user, site_name=site_name, site_url=site_url, username=username, email=email,
                 password=password).exists()
@@ -55,6 +55,62 @@ class AddPasswordView(APIView):
 
             # return password data
             password_ser = FullPasswordSerializer(new_password)
+            return Response(password_ser.data)
+
+        return Response(serializer.errors)
+
+
+class UpdatePasswordView(APIView):
+    def put(self, request, id):
+        serializer = PasswordSerializer(request.data)
+        if serializer.is_valid():
+            user = request.user
+
+            # check if we have the user and also if it is the authenticated user
+            if not user:
+                response = {'detail': 'user NOT found!'}
+                return Response(response)
+            if user.is_anonymous:
+                raise AuthenticationFailed('unauthenticated!')
+
+            site_name = request.data['site_name']
+            site_url = request.data['site_url']
+            username = request.data['username']
+            email = request.data['email']
+            account_password = request.data['password']
+            user_password = request.data['user_password']
+
+            # checking user entered password
+            if not user.check_password(user_password):
+                response = {'detail': 'user password invalid!'}
+                return Response(response)
+
+            # checking if the password exist
+            password = Passwords.objects.get(id=id)
+            if not password:
+                response = {'detail': 'password NOT found'}
+                return Response(response)
+
+            # checking if the exact information are already in the database
+            is_exist = Passwords.objects.filter(
+                user=user, site_name=site_name, site_url=site_url, username=username, email=email,
+                password=account_password).exists()
+
+            if is_exist:
+                response = {'detail': 'The info is already saved!'}
+                return Response(response)
+
+            # setting new values for the Password
+            password.site_name = site_name
+            password.site_url = site_url
+            password.username = username
+            password.email = email
+            password.password = account_password
+
+            password.save()
+
+            # return password data
+            password_ser = FullPasswordSerializer(password)
             return Response(password_ser.data)
 
         return Response(serializer.errors)
